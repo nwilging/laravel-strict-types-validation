2820a4459b995c6c5820066f6e23df3e4089a431
Nick Wilging <nick@epautomotive.org>
2022-05-24 13:32:46 -0400

    just chown holy shit

--- .github/scripts/pre-test.sh
+++ .github/scripts/pre-test.sh
@@ -1,7 +1,2 @@
 #!/bin/sh -v
-find ./vendor/orchestra/testbench-core/laravel/storage -type d -exec chgrp -v www-data {} +
-find ./vendor/orchestra/testbench-core/laravel/storage -type d -exec chmod ug+rw {} +
-find ./vendor/orchestra/testbench-core/laravel/bootstrap/cache -type d -exec chgrp -v www-data {} +
-find ./vendor/orchestra/testbench-core/laravel/bootstrap/cache -type d -exec chmod ug+rw {} +
-chgrp -v www-data ./vendor/orchestra/testbench-core/laravel/bootstrap/cache/services.php
-chmod ug+rw ./vendor/orchestra/testbench-core/laravel/bootstrap/cache/services.php
+chown -R $(whoami) vendor/
